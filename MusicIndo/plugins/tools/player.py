d6d9f132a6d768fbd811ac08751cb7aff54d5af9
Renz <alightmotionprem229@gmail.com>
2024-12-23 13:24:35 +0700

    up

--- MusicIndo/plugins/tools/player.py
+++ MusicIndo/plugins/tools/player.py
@@ -16,7 +16,6 @@ from MusicIndo.utils.decorators import AdminRightsCheck
 
 from config import BANNED_USERS
 
-
 @app.on_message(
     filters.command(["cplayer", "playing", "cplaying", "player"])
     & filters.group
