59116672407400f132c0ddbd709cd14d59f6929b
Renz <alightmotionprem229@gmail.com>
2024-12-28 13:13:17 +0700

    up

--- MusicIndo/plugins/bot/func.py
+++ MusicIndo/plugins/bot/func.py
@@ -1,14 +1,11 @@
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import UserNotParticipant
 from config import MUST_JOIN
-from pyrogram import filters, Client
-from pyrogram.types import Message
+from pyrogram import filters
 
 async def subscribed(filters, client, update):
     user_id = update.from_user.id
-    if user_id in MUST_JOIN:
-        return True
-
+    
     try:
         member = await client.get_chat_member(chat_id=MUST_JOIN, user_id=user_id)
         return member.status in [
