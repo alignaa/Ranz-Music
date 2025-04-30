106b8f577e1299e9520c9efcd9f5109b4b08bb5e
Renz <alightmotionprem229@gmail.com>
2025-04-30 20:49:30 +0700

    UP

--- config.py
+++ config.py
@@ -45,11 +45,9 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Ranzzsupport")
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
-
 # Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
-
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
